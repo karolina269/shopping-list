4b17879574cd1174ea087e2dbc630628bf7c31f1
karolina269 <120135313+karolina269@users.noreply.github.com>
2023-03-24 12:33:28 +0100

    closing menu when clicked outside, named functions replaced with anonymous functions

--- scripts.js
+++ scripts.js
@@ -1,37 +1,38 @@
-const toggleMenu = () => {
-  document.querySelector(".nav__menu").classList.toggle("open");
-};
+let menu = document.querySelector(".nav__menu");
 
-const toggleSettingsMenu = () => {
+document.querySelector(".nav__toggle").addEventListener("click", (event) => {
+  event.stopPropagation();
+  menu.classList.toggle("open");
+});
+
+document.addEventListener("click", () => menu.classList.remove("open"));
+
+menu.addEventListener("click", (event) => event.stopPropagation());
+
+document.querySelector(".nav__menu-item__dropdown").addEventListener("click", () => {
   document.querySelector(".nav__sub-menu").classList.toggle("open");
-};
+});
 
-const darkOn = document.getElementById("dark-mode-on");
-const darkOff = document.getElementById("dark-mode-off");
+const darkOn = document.querySelector(".dark-on");
+const darkOff = document.querySelector(".dark-off");
 
-const toggleDarkMode = () => {
-  if (darkOn.style.display === "inline-block") {
-    darkOn.style.display = "none";
-    darkOff.style.display = "inline-block";
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    darkOn.style.display = "inline-block";
-    darkOff.style.display = "none";
+document.getElementById("dark-mode").addEventListener("click", () => {
+  if (darkOn.classList.contains("visible")) {
+    darkOn.classList.remove("visible");
+    darkOff.classList.add("visible");
     document.documentElement.setAttribute("data-theme", "");
     localStorage.setItem("theme", "");
+  } else {
+    darkOn.classList.add("visible");
+    darkOff.classList.remove("visible");
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
   }
-};
+});
 
 let currentTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : "";
 document.documentElement.setAttribute("data-theme", currentTheme);
 if (currentTheme === "dark") {
-  darkOn.style.display = "none";
-  darkOff.style.display = "inline-block";
+  darkOn.classList.add("visible");
+  darkOff.classList.remove("visible");
 }
-
-document.querySelector(".nav__toggle").addEventListener("click", toggleMenu);
-
-document.querySelector(".nav__menu-item__dropdown").addEventListener("click", toggleSettingsMenu);
-
-document.getElementById("dark-mode").addEventListener("click", toggleDarkMode);
